cb66e462c7ab1b200871f1c67a25c8b4c368e0aa
NNNestor <165709788+NNNestor@users.noreply.github.com>
2024-04-01 16:10:12 -0300

    Create leeme.lua
    me gusta jugar Gmod soy uruguayo no se que más escribir

---
+++ leeme.lua
@@ -0,0 +1,17 @@
+function ENT:information
+👋Hola! mi nombre es @NNNestor
+Estoy interesado en crear nextbots y aprender lua 5.1
+Actualmente estoy aprendiendo lua 5.1
+Estoy buscando programadores expertos en lua para ayudarme con un proyecto de Gmod
+Мне нравится создавать некстботов
+Fun fact: jugar Gmod!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    end
+end
+
+
+
+suscribete a mi canal si no lo has hecho aún
+suscribete los hace felices 😊😊
+
+canal:
+https://www.youtube.com/@NN_Nestor
